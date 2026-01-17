35f8ef9bcd5f6d32d4e90efbcd8bda68ef08d15d
Veera Raghava Morkonda <veeraraghava.morkonda@gmail.com>
2026-01-16 23:47:49 -0600

    second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Azure! This is my first Flask web app."
+    return "Hello, Iam Learning Azure."
 
 if __name__ == "__main__":
     app.run()
